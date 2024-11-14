1f297ba3f5de4a0b5b8260e86b96bc97a1d36ba6
Makhlouf Hennine <98408710+makhlouf1102@users.noreply.github.com>
2024-11-13 21:00:07 -0500

    Update two-sum.go

--- easy/two-sum.go
+++ easy/two-sum.go
@@ -4,7 +4,7 @@ func twoSum(nums []int, target int) []int {
 	subs := make(map[int]int)
 
 	for i, num := range nums {
-		if j, err := subs[num]; err != nil {
+		if j, ok := subs[num]; ok {
 			return []int{j, i}
 		}
 		subs[target-num] = i
